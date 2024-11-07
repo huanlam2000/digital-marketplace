f8d269946e1143d4703484e14f5676b73f53ab3a
lâm thế huân <huantyn@gmail.com>
2024-11-07 22:13:41 +0700

    chore: remove console log

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,6 +1,5 @@
 import Stripe from "stripe";
 
-console.log("stripe run", process.env.STRIPE_SECRET_KEY);
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? "", {
   apiVersion: "2024-10-28.acacia",
   typescript: true,
